4b0ae70ccfc2b94365b0f7e104ae447af1ee4bb8
Stefan Kernjak <stefan@thisotherthing.co>
2021-12-05 21:53:05 +0100

    finished day 4

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,8 +1,8 @@
 fn main () {
-    dbg!(part_a(include_str!("example.txt")));
-    // dbg!(part_a(include_str!("input.txt")));
+    // dbg!(part_a(include_str!("example.txt")));
+    dbg!(part_a(include_str!("input.txt")));
     // dbg!(part_b(include_str!("example.txt")));
-    // dbg!(part_b(include_str!("input.txt")));
+    dbg!(part_b(include_str!("input.txt")));
 }
 
 pub fn part_a(input: &str) -> i64 {
@@ -44,18 +44,25 @@ pub fn part_a(input: &str) -> i64 {
     gamma * epsilon
 }
 
-fn part_b(input: &str) -> i64 {
+pub fn part_b(input: &str) -> i64 {
     let numbers :Vec<&str> = input.trim().split('\n').collect();
 
-    let ox = runFilter(&numbers, true);
-    let co = runFilter(&numbers, false);
+    let ox = run_filter(&numbers, true);
+    let co = run_filter(&numbers, false);
 
-    println!("{} {}", ox, co);
+    let ox_v = i64::from_str_radix(numbers[ox], 2).expect("error parsing");
+    let co_v = i64::from_str_radix(numbers[co], 2).expect("error parsing");
 
-    ox * co
+    // dbg!(ox);
+
+    println!("{} {} {}", ox, numbers[ox], ox_v);
+    println!("{} {} {}", co, numbers[co], co_v);
+
+    // ox * co
+    ox_v * co_v
 }
 
-fn runFilter(numbers: &Vec<&str>, _high: bool) -> i64 {
+fn run_filter(numbers: &[&str], high: bool) -> usize {
 
     let num_lines = numbers.len();
     let num_chars = numbers[0].chars().count();
@@ -64,9 +71,47 @@ fn runFilter(numbers: &Vec<&str>, _high: bool) -> i64 {
 
     for i in 0..num_lines { indices[i] = i; }
 
-    let charIndex = 0;
+    // dbg!(indices);
 
-    // while 
+    let mut char_index = 0;
 
-    0
+    for _ in 0..num_chars {
+        let mut count = 0;
+
+        for i in 0..indices.len() {
+            match numbers[indices[i]].chars().nth(char_index).unwrap() {
+                '0' => { count -= 1; },
+                '1' => { count += 1; },
+                _ => panic!(),
+            }
+        }
+
+        let char_to_match: char;
+
+        if high {
+            if count >= 0 {
+                char_to_match = '1';
+            } else {
+                char_to_match = '0';
+            }
+        } else {
+            if count >= 0 {
+                char_to_match = '0';
+            } else {
+                char_to_match = '1';
+            }
+        }
+
+        indices.retain(|&idx| {
+            return numbers[idx].chars().nth(char_index).unwrap() == char_to_match
+        });
+
+        if indices.len() == 1 {
+            return indices[0]
+        }
+
+        char_index += 1;
+    }
+
+    panic!()
 }
---
+++ day4/src/main.rs
@@ -0,0 +1,134 @@
+fn main () {
+    // dbg!(part_a(include_str!("example.txt")));
+    // dbg!(part_a(include_str!("input.txt")));
+    dbg!(part_b(include_str!("example.txt")));
+    dbg!(part_b(include_str!("input.txt")));
+}
+
+struct Board {
+    numbers: Vec<u32>,
+    checked: Vec<bool>,
+    w: usize,
+    h: usize,
+    l: usize
+}
+
+fn get_board(input: &str) -> Board {
+    let lines: Vec<&str> = input.trim().split('\n').collect();
+
+    let h = lines.len();
+
+    let long_input = input.trim().replace('\n', " ");
+
+    let numbers: Vec<u32> = long_input.split(' ').filter(|s| !s.is_empty()).map(|s| s.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+
+    let num_numbers = numbers.len();
+
+    let w = num_numbers / lines.len();
+
+    // dbg!(numbers);
+    // dbg!((num_numbers / lines.len()) as u32);
+    // dbg!(h);
+
+    Board {
+        w,
+        h,
+        numbers,
+        checked: vec![false; num_numbers],
+        l: w * h,
+
+    }
+}
+
+fn check_board_win(board: &Board) -> bool {
+    // check down
+    for x in 0..board.w {
+        if board.checked.iter().skip(x).step_by(board.w).all(|checked| *checked) { return true; }
+    }
+
+    // check sideways
+    for y in 0..board.h {
+        if board.checked.iter().skip(y * board.w).take(board.w).all(|checked| *checked) { return true; }
+    }
+
+    false
+}
+
+fn update_board(board: &mut Board, number: u32) {
+    for i in 0..board.l {
+        if !board.checked[i] && board.numbers[i] == number { board.checked[i] = true; }
+    }
+}
+
+pub fn part_a(input: &str) -> u32 {
+    let mut data_chunks= input.trim().split("\n\n");
+
+    let numbers: Vec<u32>= data_chunks.next().unwrap().split(',').map(|i| i.parse::<u32>().unwrap()).collect();
+
+    let mut boards: Vec<Board> = data_chunks.map(get_board).collect();
+
+    // dbg!(numbers);
+    // dbg!(boards);
+
+    for number in numbers {
+        println!("{}", number);
+
+        for board in &mut boards {
+            update_board(board, number);
+            if check_board_win(board) {
+                let mut result: u32 = 0;
+
+                for i in 0..board.l {
+                    if !board.checked[i] { result += board.numbers[i]; }
+                }
+
+                return result * number;
+            }
+        }
+    }
+
+    0
+}
+
+pub fn part_b(input: &str) -> u32 {
+    let mut data_chunks= input.trim().split("\n\n");
+
+    let numbers: Vec<u32>= data_chunks.next().unwrap().split(',').map(|i| i.parse::<u32>().unwrap()).collect();
+
+    let mut boards: Vec<Board> = data_chunks.map(get_board).collect();
+
+    let mut solved: Vec<bool> = vec![Default::default(); boards.len()];
+
+    let mut last_result = 0;
+
+    for number in numbers {
+        // println!("{}", number);
+
+        for i in 0..boards.len() {
+            update_board(&mut boards[i], number);
+            if check_board_win(&boards[i]) && !solved[i] {
+                let mut result: u32 = 0;
+
+                for j in 0..boards[i].l {
+                    if !boards[i].checked[j] { result += boards[i].numbers[j]; }
+                }
+
+                last_result = result * number;
+
+                solved[i] = true;
+
+                println!("solved {}: {}", i, last_result);
+            }
+
+            if !solved.contains(&false) { return last_result; }
+        }
+    }
+
+    last_result
+}
+
+// pub fn part_b(input: &str) -> i64 {
+//     let numbers :Vec<&str> = input.trim().split('\n').collect();
+
+//     0
+// }
